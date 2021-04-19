4277f36bee52d0dc248d7b349f0361df4fa6157f
stefanscript <sha32768@gmail.com>
2021-04-19 18:29:08 +0100

    DishwasherList can display multiple dishwashers

--- src/components/DishwashersList.spec.tsx
+++ src/components/DishwashersList.spec.tsx
@@ -3,37 +3,52 @@ import DishwashersList, { Dishwasher } from "./DishwashersList";
 import { render, screen, within } from "@testing-library/react";
 
 describe("DishwashersList List", () => {
+    const boschDishwasher = {
+        productId: "1955287",
+        prize: {
+            now: "397.00",
+        },
+        title: "Bosch Serie 2 SMV40C30GB Fully Integrated Dishwasher",
+        image: "//johnlewis.scene7.com/is/image/JohnLewis/234378764?",
+    };
+
+    const siemensDishwasher = {
+        productId: "4189551",
+        prize: {
+            now: "549.00",
+        },
+        title: "Siemens iQ300 SN236W03MG Freestanding Dishwasher, White",
+        image: "//johnlewis.scene7.com/is/image/JohnLewis/238008972?",
+    };
+
     it("should display No dishwashers available", () => {
         render(<DishwashersList dishwashers={[]} />);
 
         expect(screen.getByText("No dishwashers available")).toBeInTheDocument();
     });
 
-    describe("With one dishwasher", () => {
-        let dishwasher: Dishwasher = {
-            productId: "1955287",
-            prize: {
-                now: "397.00",
-            },
-            title: "Bosch Serie 2 SMV40C30GB Fully Integrated Dishwasher",
-            image: "//johnlewis.scene7.com/is/image/JohnLewis/234378764?",
-        };
+    it("should display one dishwasher", () => {
+        render(<DishwashersList dishwashers={[boschDishwasher]} />);
 
-        it("should display one dishwasher", () => {
-            render(<DishwashersList dishwashers={[dishwasher]} />);
+        const dishwasherElement = screen.getByRole("article");
+        expect(dishwasherElement).toHaveTextContent("Bosch Serie 2 SMV40C30GB Fully Integrated Dishwasher");
+        expect(dishwasherElement).toHaveTextContent("£397.00");
+        expect(
+            within(dishwasherElement).getByAltText("Bosch Serie 2 SMV40C30GB Fully Integrated Dishwasher")
+        ).toHaveAttribute("src", "//johnlewis.scene7.com/is/image/JohnLewis/234378764?");
+    });
 
-            const dishwasherElement = screen.getByRole("article");
-            expect(dishwasherElement).toHaveTextContent("Bosch Serie 2 SMV40C30GB Fully Integrated Dishwasher");
-            expect(dishwasherElement).toHaveTextContent("£397.00");
-            expect(
-                within(dishwasherElement).getByAltText("Bosch Serie 2 SMV40C30GB Fully Integrated Dishwasher")
-            ).toHaveAttribute("src", "//johnlewis.scene7.com/is/image/JohnLewis/234378764?");
-        });
+    it("should display multiple dishwashers", () => {
+        render(<DishwashersList dishwashers={[boschDishwasher, siemensDishwasher]} />);
 
-        it("should not display the 'no dishwashers available' message", () => {
-            render(<DishwashersList dishwashers={[dishwasher]} />);
+        expect(screen.getAllByRole("article")).toHaveLength(2);
+        expect(screen.getByText("Bosch Serie 2 SMV40C30GB Fully Integrated Dishwasher")).toBeInTheDocument();
+        expect(screen.getByText("Siemens iQ300 SN236W03MG Freestanding Dishwasher, White")).toBeInTheDocument();
+    });
 
-            expect(screen.queryByText("No dishwashers available")).not.toBeInTheDocument();
-        });
+    it("should not display the 'no dishwashers available' message when more than one dishwasher", () => {
+        render(<DishwashersList dishwashers={[boschDishwasher]} />);
+
+        expect(screen.queryByText("No dishwashers available")).not.toBeInTheDocument();
     });
 });
