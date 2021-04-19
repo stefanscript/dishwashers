f16fa99bf66484ef7c6022e30650dcca5e970868
stefanscript <sha32768@gmail.com>
2021-04-19 07:58:50 +0100

    write acceptance test for displaying the first 20 dishwashers

--- test/acceptance/index.spec.tsx
+++ test/acceptance/index.spec.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import Home from "../../src/pages";
+import Home, { Product } from "../../src/pages";
+import productResponse from "../dishwashers.data";
 
 describe("Given we are on the Dishwashers Home Page", () => {
     describe("And there are no dishwashers", () => {
@@ -10,4 +11,16 @@ describe("Given we are on the Dishwashers Home Page", () => {
             expect(screen.getByText(/No dishwashers available/)).toBeInTheDocument();
         });
     });
+
+    xdescribe("And there are dishwashers", () => {
+        it("Then we should see the first 20 dishwashers", () => {
+            const products: Product[] = productResponse.products;
+
+            render(<Home products={products}/>);
+
+            expect(screen.getAllByRole("article").length).toEqual(20);
+        });
+    });
 });
+
+
