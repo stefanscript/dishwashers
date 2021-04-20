db5b59d7e0ca45625ff0f9bdc21803561fe534ca
stefanscript <sha32768@gmail.com>
2021-04-20 13:02:52 +0100

    style dishwasher details page - portrait

--- src/components/DishwasherDetails/DishwasherDetails.tsx
+++ src/components/DishwasherDetails/DishwasherDetails.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { DishwasherDetailsInterface } from "../../pages/dishwasher/[productId]";
+import styles from "./dishwasherdetails.module.css";
 
 interface Props {
     details: DishwasherDetailsInterface;
@@ -7,29 +8,35 @@ interface Props {
 
 const DishwasherDetails: React.FC<Props> = ({ details }) => {
     return (
-        <>
-            <img src={details.media.images.urls[0]} alt={details.media.images.altText} />
-            <div>&pound;{details.price.now}</div>
-            <div>{details.displaySpecialOffer}</div>
-            <div>{details.additionalServices.includedServices}</div>
-            <div>
-                <h2>Product Information</h2>
-                <h3>Product code: {details.code}</h3>
-                <div dangerouslySetInnerHTML={{ __html: details.details.productInformation }} />
+        <div className={styles.container}>
+            <div className={styles.carousel}>
+                <img src={details.media.images.urls[0]} alt={details.media.images.altText} />
             </div>
-            <div>
-                <h2>Product Specification</h2>
-                <ul>
-                    {details.features.map((spec) => (
-                        <li key={spec.name}>
-                            <div>{spec.name}</div>
-                            &nbsp;
-                            <div>{spec.value}</div>
-                        </li>
-                    ))}
-                </ul>
+            <div className={styles.separator} />
+            <div className={styles.wrapper}>
+                <div className={styles.price}>&pound;{details.price.now}</div>
+                <div className={styles.offer}>{details.displaySpecialOffer}</div>
+                <div>{details.additionalServices.includedServices}</div>
+                <div className={styles.productInfo}>
+                    <h2>Product Information</h2>
+                    <div dangerouslySetInnerHTML={{ __html: details.details.productInformation }} />
+                    <div>Product code: {details.code}</div>
+                </div>
+
+                <div>
+                    <h2 className={styles.specTitle}>Product Specification</h2>
+                    <ul className={styles.specList}>
+                        {details.features.map((spec) => (
+                            <li key={spec.name} className={styles.specListItem}>
+                                <div>{spec.name}</div>
+                                &nbsp;
+                                <div>{spec.value}</div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
-        </>
+        </div>
     );
 };
 
